3d7315efce47f1923a74534c1214817cf7206a12
Jorge Abad <jorgejoseabad@gmail.com>
2017-02-28 17:37:34 +0100

    first iteration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MyFoodsComponent } from './my-foods/my-foods.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFoodsComponent
   ],
   imports: [
     BrowserModule,
